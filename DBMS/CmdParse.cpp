da4af810f83adcb16bcd8486cbded0c397cce4e7
SeulYoung <948170997@qq.com>
2018-11-07 17:05:29 +0800

    cmdParse

--- DBMS/CmdParse.cpp
+++ DBMS/CmdParse.cpp
@@ -10,3 +10,28 @@ CmdParse::CmdParse()
 CmdParse::~CmdParse()
 {
 }
+
+string CmdParse::sqlCheck(string sql)
+{
+	regex Pupdate("update\.+set\.+(where\.+)?END");
+	regex Pdelete("delete\.+from\.+where\.+END");
+	regex Pinsert("insert\.+into\.+(\(\.+/)\.*)?values\(.+\)END");
+	regex Pcreate("create table\.+\\(\.+\\)END");
+	regex Pselect("select\.+from\.+(where\.+)?((group by)?|(order by)?|(having)?)END");
+	regex Pdrop("drop table\.+END");
+
+	if (regex_match(sql, Pupdate))
+		return  ForUpdate();
+	else if (regex_match(sql, Pdelete))
+		return  ForDelete();
+	else if (regex_match(sql, Pinsert))
+		return ForInsert();
+	else if (regex_match(sql, Pcreate))
+		return ForCreate();
+	else if (regex_search(sql, Pselect))
+		return ForSelect();
+	else if (regex_match(sql, Pdrop))
+		return ForDrop();
+
+	return "Sql”Ôæ‰¥ÌŒÛ";
+}
--- DBMS/CmdParse.h
+++ DBMS/CmdParse.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <regex>
+
+using namespace std;
 
 class CmdParse
 {
 public:
 	CmdParse();
 	~CmdParse();
+	string sqlCheck(string sql);
 
 private:
 
